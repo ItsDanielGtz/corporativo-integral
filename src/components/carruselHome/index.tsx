350fef7122106868c2f77918ffbaf3a091602211
ItsDanielGtz <daniel.24@gmail.com>
2024-06-20 08:16:54 -0600

    Se modifico el componente carrousel y se agregaron nuevos estilos

--- src/components/carruselHome/index.tsx
+++ src/components/carruselHome/index.tsx
@@ -1,13 +1,12 @@
 import * as React from "react";
 import Autoplay from "embla-carousel-autoplay";
-import logo from "../../assets/images/LogoKYM.png";
-
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
+import { images } from "@/constants";
+import { Separator } from "../ui/separator";
 
 export function CarouselHome() {
   const plugin = React.useRef(
@@ -15,33 +14,38 @@ export function CarouselHome() {
   );
 
   return (
-    <Carousel
-      plugins={[plugin.current]}
-      className="w-full h-[80dvh] overflow-hidden"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
-      <CarouselContent className="flex flex-1 w-full">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="">
-              <Card>
-                <CardContent className="flex aspect-square items-start justify-start p-6">
-                  <img
-                    src={logo}
-                    width={700}
-                    height={700}
-                    alt="Carousel Image 1"
-                    className=" object-cover"
-                  />
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      {/* <CarouselPrevious />
-      <CarouselNext /> */}
-    </Carousel>
+    <div className="relative">
+      <Carousel
+        plugins={[plugin.current]}
+        className="basis-0 flex-grow w-full"
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent className="flex flex-1 ">
+          {images.map(({ image, alt }) => (
+            <CarouselItem key={alt} className="w-full">
+
+              <img
+                src={image}
+                alt={alt}
+                className="h-screen w-screen object-cover "
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+      <div className=" bg-gradient-to-r from-gray-900/50 absolute inset-0" />
+      <div className="absolute inset-0 flex items-center justify-start p-10">
+        <div className=" space-y-4">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold  text-white/80">
+            Corporativo Integral K&M del Bajío
+          </h1>
+          <Separator className="bg-gradient-to-r from-azul"/>
+          <p className="text-lg sm:text-xl md:text-2xl font-semibold  text-white/80">
+            International Trade Business
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
