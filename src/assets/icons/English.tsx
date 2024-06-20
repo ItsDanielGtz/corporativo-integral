3cccb6be8635f981d520cb334fb52fd6b3838384
ItsDanielGtz <daniel.24@gmail.com>
2024-06-20 08:15:02 -0600

    Se agregaron iconos para cambio de idioma

---
+++ src/assets/icons/English.tsx
@@ -0,0 +1,18 @@
+interface Props {
+  className?: string
+}
+
+function English({className}: Props) {
+  return (
+    <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 50 50"
+  >
+    <circle cx="25" cy="25" r="20" stroke="currentColor" strokeWidth="2" fill="none" />
+    <text x="17" y="30" fontFamily="Arial" fontSize="15" fill="currentColor">EN</text>
+  </svg>
+  )
+}
+
+export default English
