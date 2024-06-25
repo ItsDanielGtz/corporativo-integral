76aa0de12acff47e512864b9ca75ccd3300178d5
ItsDanielGtz <daniel.24@gmail.com>
2024-06-24 19:04:24 -0600

    Se cambio colores del icono del botón language

--- src/assets/icons/spanish.tsx
+++ src/assets/icons/spanish.tsx
@@ -1,18 +1,28 @@
 interface Props {
-  className?: string
+  className?: string;
 }
 
-function Spanish({className}: Props) {
+function Spanish({ className }: Props) {
   return (
     <svg
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 50 50"
-  >
-    <circle cx="25" cy="25" r="20" stroke="currentColor" strokeWidth="2" fill="none" />
-    <text x="17" y="30" fontFamily="Arial" fontSize="15" fill="currentColor">ES</text>
-  </svg>
-  )
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+    >
+      <circle
+        cx="25"
+        cy="25"
+        r="20"
+        stroke="currentColor"
+        strokeWidth="2"
+        fill="none"
+        color="white"
+      />
+      <text x="17" y="30" fontFamily="Arial" fontSize="15" fill="currentColor" color="white">
+        ES
+      </text>
+    </svg>
+  );
 }
 
-export default Spanish
+export default Spanish;
