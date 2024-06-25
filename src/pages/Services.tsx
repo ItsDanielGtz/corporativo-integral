4af2b3e3c442620cb640780d906f1339793f1065
ItsDanielGtz <daniel.24@gmail.com>
2024-06-24 23:05:25 -0600

    Se hicieron cambios en los estilos en la page Service

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -11,7 +11,7 @@ function Services() {
       <Seo
         title={t("seo.services.title")}
         description={t("seo.services.description")}
-        keywords={[t("seo.services.keywords", {joinArrays: ","} )]}
+        keywords={[t("seo.services.keywords", { joinArrays: "," })]}
       />
       <div className="flex flex-col  py-8 px-12 lg:gap-16 lg:grid lg:grid-cols-12">
         <div className="lg:col-span-5 ">
@@ -21,20 +21,23 @@ function Services() {
             className="object-contain items-center justify-center"
           />
         </div>
-        <div className="grid grid-cols-2 gap-x-4 pt-4 lg:col-span-7 lg:flex lg:flex-col lg-w-full">
+        <div className="pt-4 lg:col-span-7 lg:flex lg:flex-col lg-w-full">
           {services.map((service, index) => (
-            <div key={Object.keys(service)[index]}>
+            <div
+              key={Object.keys(service)[index]}
+              className="grid grid-cols-2 gap-3 lg:flex lg:flex-col lg:gap-0"
+            >
               {Object.keys(service).map((key) => {
                 const Icon = service[key as keyof typeof service].icon;
                 return (
                   <div
                     key={key}
-                    className=" flex border-b border-azul py-5  lg:first:pt-0 lg:py-5"
+                    className=" flex border-b border-azul py-5  lg:first:pt-0 lg:py-5 "
                   >
                     <div className="text-amarillo font-bold pl-3 pr-6">
                       <Icon />
                     </div>
-                    <div className=" flex-1">
+                    <div className=" flex-1 ">
                       {t(`services.${key}.description`)}
                     </div>
                   </div>
